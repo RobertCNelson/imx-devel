925a1b4fefb670687877acd8e74f486d24998a2d
Robert Nelson <robertcnelson@gmail.com>
2012-03-29 14:42:23 -0500

    now boots on the newer mx53loco R board
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -25,24 +25,20 @@
 echo "Starting patch.sh"
 
 function git_add {
-git add .
-git commit -a -m 'testing patchset'
+	git add .
+	git commit -a -m 'testing patchset'
 }
 
 function bugs_trivial {
-echo "bugs and trivial stuff"
+	echo "bugs and trivial stuff"
 }
 
 function freescale {
 echo "from freescale dump..."
+	git pull git://github.com/RobertCNelson/linux.git imx_2.6.35_11.09.01
 
-#other options: http://opensource.freescale.com/git?p=imx/linux-2.6-imx.git;a=summary
-#git pull http://opensource.freescale.com/pub/scm/imx/linux-2.6-imx.git imx_2.6.35_11.05.01
-
-git pull git://github.com/RobertCNelson/linux-2.6.git imx_2.6.35_11.05.01
-
-#Causes Serial Corruption Loco Board
-patch -p1 -s < ${DIR}/patches/0001-Revert-ENGR00141363-ARM-imx53-clock-change-di0-clock.patch
+	#Causes Serial Corruption Loco Board
+	git am "${DIR}/patches/0001-Revert-ENGR00141363-ARM-imx53-clock-change-di0-clock.patch"
 }
 
 bugs_trivial
