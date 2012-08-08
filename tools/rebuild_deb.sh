eb291bfe1e41b26a52379645338f917deb7240d2
Robert Nelson <robertcnelson@gmail.com>
2012-08-08 08:47:07 -0500

    dtbs: fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/rebuild_deb.sh
+++ tools/rebuild_deb.sh
@@ -70,7 +70,10 @@ function make_deb {
 	if [ "x${DTBS}" != "x" ] ; then
 		echo "make -j${CORES} ARCH=arm LOCALVERSION=-${BUILD} CROSS_COMPILE=\"${CCACHE} ${CC}\" ${CONFIG_DEBUG_SECTION} dtbs"
 		time make -j${CORES} ARCH=arm LOCALVERSION=-${BUILD} CROSS_COMPILE="${CCACHE} ${CC}" ${CONFIG_DEBUG_SECTION} dtbs
+		ls arch/arm/boot/* | grep dtb || unset DTBS
 	fi
+
+	KERNEL_UTS=$(cat ${DIR}/KERNEL/include/generated/utsrelease.h | awk '{print $3}' | sed 's/\"//g' )
 
 	cd ${DIR}/
 }
