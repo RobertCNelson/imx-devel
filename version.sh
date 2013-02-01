5e19a91feb9798c46ae261be1993adba52c27a23
Robert Nelson <robertcnelson@gmail.com>
2013-01-31 20:49:49 -0600

    3.8-rc6-imx2 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,8 +23,8 @@ config="imx_v6_v7_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=imx1
+KERNEL_TAG=${KERNEL_REL}-rc6
+BUILD=imx2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
