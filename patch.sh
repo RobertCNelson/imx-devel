406fcaaff1cebedd6f11ccf32ae890a67265dde5
Robert Nelson <robertcnelson@gmail.com>
2013-04-24 12:15:02 -0500

    we need a random mac on some devices
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -77,6 +77,7 @@ imx () {
 	${git} "${DIR}/patches/imx/0003-imx-Add-IMX53-AHCI-support.patch"
 	${git} "${DIR}/patches/imx/0005-SAUCE-imx6-enable-sata-clk-if-SATA_AHCI_PLATFORM.patch"
 #	${git} "${DIR}/patches/imx/0005-staging-imx-drm-request-irq-only-after-adding-the-cr.patch"
+	${git} "${DIR}/patches/imx/0006-arm-fec-use-random-mac-when-everything-else-fails.patch"
 }
 
 chipidea () {
