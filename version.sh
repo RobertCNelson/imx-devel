4161f1eb4a3f3a5f0b3b1a9d3ff4d30a1e813ac4
Robert Nelson <robertcnelson@gmail.com>
2013-07-27 17:05:06 -0500

    kernel bump: v3.4.54
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,8 +27,8 @@ linaro_toolchain="cortex_gcc_4_7"
 
 #Kernel/Build
 KERNEL_REL=3.4
-KERNEL_TAG=${KERNEL_REL}.20
-BUILD=imx2
+KERNEL_TAG=${KERNEL_REL}.54
+BUILD=imx2.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
